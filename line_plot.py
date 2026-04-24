6ce4593ade87091db6ed0e71693a81a255c1f335
Shridhar <shridhartapakire4051@gmail.com>
2026-04-24 21:00:08 +0530

    Seaborn: line plot implementation

---
+++ line_plot.py
@@ -0,0 +1,15 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Data
+x = [1, 2, 3, 4, 5]
+y = [10, 20, 30, 40, 50]
+
+# Line plot
+sns.lineplot(x=x, y=y)
+
+plt.title("Seaborn Line Plot")
+plt.xlabel("X Values")
+plt.ylabel("Y Values")
+
+plt.show()
