54ff754099071395aeedd3a2099a9c67d5d05aae
Shridhar <shridhartapakire4051@gmail.com>
2026-04-26 23:02:00 +0530

    Seaborn: scatter plot for relationship visualization

---
+++ scatter_plot.py
@@ -0,0 +1,15 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Data
+x = [1, 2, 3, 4, 5]
+y = [5, 15, 20, 25, 30]
+
+# Scatter plot
+sns.scatterplot(x=x, y=y)
+
+plt.title("Seaborn Scatter Plot")
+plt.xlabel("X Values")
+plt.ylabel("Y Values")
+
+plt.show()
