636540515991c1aa43ffec899c6638e125bbe9be
Shridhar <shridhartapakire4051@gmail.com>
2026-04-27 22:43:06 +0530

    Seaborn: histogram for data distribution

---
+++ histogram.py
@@ -0,0 +1,14 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Data
+expenses = [100, 200, 150, 300, 250, 400, 350, 200, 150, 300]
+
+# Histogram
+sns.histplot(expenses, bins=5)
+
+plt.title("Expense Distribution")
+plt.xlabel("Amount")
+plt.ylabel("Frequency")
+
+plt.show()
